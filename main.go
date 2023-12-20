a3b69e02a40f423f6dc233b9ed11745e8b780a28
Thomas Bruyelle <thomas.bruyelle@tendermint.com>
2023-12-20 22:40:22 +0100

    first commit

---
+++ main.go
@@ -0,0 +1,116 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/gogo/protobuf/jsonpb"
+
+	codectypes "github.com/cosmos/cosmos-sdk/codec/types"
+	cryptocodec "github.com/cosmos/cosmos-sdk/crypto/codec"
+	govtypes "github.com/cosmos/cosmos-sdk/x/gov/types"
+	stakingtypes "github.com/cosmos/cosmos-sdk/x/staking/types"
+)
+
+var unmarshaler jsonpb.Unmarshaler
+
+func init() {
+	registry := codectypes.NewInterfaceRegistry()
+	cryptocodec.RegisterInterfaces(registry)
+	unmarshaler = jsonpb.Unmarshaler{AnyResolver: registry}
+}
+
+func main() {
+	// Read data from files
+	datapath := os.Args[1]
+	votes, err := parseVotes(datapath)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%d votes\n", len(votes))
+	valsByAddr, err := parseValidatorsByAddr(datapath)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%d validators\n", len(valsByAddr))
+	delegsByAddr, err := parseDelegationsByAddr(datapath)
+	if err != nil {
+		panic(err)
+	}
+	var numDeleg int
+	for _, d := range delegsByAddr {
+		numDeleg += len(d)
+	}
+	fmt.Printf("%d delegations for %d delegators\n", numDeleg, len(delegsByAddr))
+
+	// Compute total voting power
+	// var vp uint64
+	// for _, v := range votes {
+	// }
+}
+
+func parseVotes(path string) (govtypes.Votes, error) {
+	f, err := os.Open(filepath.Join(path, "votes_final.json"))
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+	dec := json.NewDecoder(f)
+	_, err = dec.Token()
+	if err != nil {
+		return nil, err
+	}
+	var votes govtypes.Votes
+	for dec.More() {
+		var vote govtypes.Vote
+		err := unmarshaler.UnmarshalNext(dec, &vote)
+		if err != nil {
+			return nil, err
+		}
+		votes = append(votes, vote)
+	}
+	return votes, nil
+}
+
+func parseDelegationsByAddr(path string) (map[string][]stakingtypes.Delegation, error) {
+	f, err := os.Open(filepath.Join(path, "delegations.json"))
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+	var delegs []stakingtypes.Delegation
+	err = json.NewDecoder(f).Decode(&delegs)
+	if err != nil {
+		return nil, err
+	}
+	delegsByAddr := make(map[string][]stakingtypes.Delegation)
+	for _, d := range delegs {
+		delegsByAddr[d.DelegatorAddress] = append(delegsByAddr[d.DelegatorAddress], d)
+	}
+	return delegsByAddr, nil
+}
+
+func parseValidatorsByAddr(path string) (map[string]stakingtypes.Validator, error) {
+	f, err := os.Open(filepath.Join(path, "active_validators.json"))
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+	dec := json.NewDecoder(f)
+	_, err = dec.Token()
+	if err != nil {
+		return nil, err
+	}
+	valsByAddr := make(map[string]stakingtypes.Validator)
+	for dec.More() {
+		var val stakingtypes.Validator
+		err := unmarshaler.UnmarshalNext(dec, &val)
+		if err != nil {
+			return nil, err
+		}
+		valsByAddr[val.OperatorAddress] = val
+	}
+	return valsByAddr, nil
+}
